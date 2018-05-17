2c0547d8669953cee4262589bc70451d2b128aa0
mhlan <33137338+mhlan@users.noreply.github.com>
2018-05-17 16:00:27 -0700

    Added missing files

--- js/main.js
+++ js/main.js
@@ -4,6 +4,3 @@ const masterList = [...document.querySelectorAll(".student-item")];
 const pageNum = Math.round(masterList.length / 10);
 //static number of students to be shown per page
 const numPerPage = 10;
-
-// const pagination =
-//   ' <div class="pagination"><ul><li><a class="active" href="#">1</a></li></ul ></div >';
